6672e44c150e046367fd97fdd9a124bf93495cb5
manyimy <wong.manyi19@gmail.com>
2022-09-25 07:00:02 +0800

    NewRecord Expense/Income Tabs

--- lib/addEntryDialog.dart
+++ lib/addEntryDialog.dart
@@ -10,19 +10,33 @@ class AddEntryDialog extends StatefulWidget {
 class AddEntryDialogState extends State<AddEntryDialog> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('New entry'),
-        actions: [
-          TextButton(
-              onPressed: () {
-                //TODO: Handle save
-              },
-              child: Text('SAVE',
-                  style: Theme.of(context).textTheme.subtitle1?.copyWith(color: Colors.white))),
-        ],
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: 'Expense'),
+              Tab(text: 'Income'),
+            ],
+          ),
+          title: const Text('New Record'),
+          // actions: [
+          //   TextButton(
+          //       onPressed: () {
+          //         //TODO: Handle save
+          //       },
+          //       child: Text('SAVE',
+          //           style: Theme.of(context).textTheme.subtitle1?.copyWith(color: Colors.white))),
+          // ],
+        ),
+        // body: const TabBarView(
+        //   children: [
+        //     Icon(Icons.directions_car),
+        //     Icon(Icons.directions_transit),
+        //   ],
+        // ),
       ),
-      body: const Text("Foo"),
     );
   }
 }
